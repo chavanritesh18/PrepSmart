287b6e8e3e7ca790e46d8474c4d1c7b8c9155faf
Chavan Ritesh <140919594+chavanritesh18@users.noreply.github.com>
2024-06-13 15:22:26 +0530

    Update page.jsx

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -63,12 +63,11 @@ export default function Page() {
           </a>
 
           <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-            Welcome to MOCK INTERVIEW BY AI
+            Welcome to PrepStart.io
           </h1>
 
           <p className="mt-4 leading-relaxed text-gray-500">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-            quibusdam aperiam voluptatum.
+            Prepare, Practice, and Perfect Your AI Interview Skills
           </p>
         </div>
 
