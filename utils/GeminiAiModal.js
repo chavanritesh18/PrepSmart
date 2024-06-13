066615ef1a10d3563ab1f79f0b6e9fea698ef994
Chavan Ritesh <140919594+chavanritesh18@users.noreply.github.com>
2024-06-13 13:59:19 +0530

    Update GeminiAiModal.js
    Again the js code of the file is uploaded

--- utils/GeminiAiModal.js
+++ utils/GeminiAiModal.js
@@ -8,8 +8,7 @@ const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.0-flash",
-  
+  model: "gemini-1.5-flash",
 });
 
 const generationConfig = {
@@ -38,11 +37,8 @@ const safetySettings = [
     threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
   },
 ];
-
-
   export const chatSession = model.startChat({
     generationConfig,
-    safetySettings,
+     safetySettings,
   });
-
 
